817a2b7ea50dd130e2df5144d009f25d45ac15a3
Lukas Mikulenas <LMikulenas@ucvts.tec.nj.us>
2020-02-25 12:52:19 -0500

    js/app2brickbreakers.js

--- js/app2brickbreakers.js
+++ js/app2brickbreakers.js
@@ -1,5 +1,11 @@
 ///////////////////// CONSTANTS /////////////////////////////////////
-var ctx = document.getElementById("brick-canvas").getContext("2d");
+var canvas = document.getElementById("brick-canvas")
+var ctx= canvas.getContext("2d");
+var x = canvas.width/2;
+var y = canvas.height-30;
+var dx = 2;
+var dy = -2;
+var ballRadius = 10;
 ///////////////////// APP STATE (VARIABLES) /////////////////////////
 
 ///////////////////// CACHED ELEMENT REFERENCES /////////////////////
@@ -12,8 +18,27 @@ function canvasLoad() {
 }
 function canvasDraw() {
   ctx.beginPath();
-  ctx.arc(240, 160, 20, 0, Math.PI*2, false);
-  ctx.fillStyle = "green";
+  ctx.rect(20, 40, 50, 50);
+  ctx.fillStyle = "#FF0000";
   ctx.fill();
+  drawBall();
+  draw();
   ctx.closePath();
+}
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.fillStyle = "#82261f";
+    ctx.fill();
+    ctx.closePath();
+    setInterval(draw, 10);
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    x += dx;
+    y += dy;
+    setInterval(draw, 10);
 }
