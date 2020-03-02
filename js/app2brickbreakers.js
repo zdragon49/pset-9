ef4dffbf771a35a310e395722d38c6a3eb2f8bf9
Lukas Mikulenas <lmikulenas@ucvts.org>
2020-03-01 20:50:28 -0500

    I FORGOT ABOUT THIS

--- js/app2brickbreakers.js
+++ js/app2brickbreakers.js
@@ -1,76 +1,47 @@
-///////////////////// CONSTANTS /////////////////////////////////////
-var canvas = document.getElementById("brick-canvas")
-var ctx= canvas.getContext("2d");
-var x = canvas.width/2;
-var y = canvas.height-30;
-var dx = 2;
-var dy = -2;
-var ballRadius = 10;
-var colorBall = 0;
-///////////////////// APP STATE (VARIABLES) /////////////////////////
 
-///////////////////// CACHED ELEMENT REFERENCES /////////////////////
+// Canvas
+const canvas = document.getElementById("brick-breaker-game");
+const ctx = canvas.getContext("2d");
+ctx.fillStyle = "red";
+ctx.strokeStyle = "green";
+ctx.lineWidth = 2.5;
+ctx.lineCap = "round";
+ctx.lineJoin = "round";
+ctx.textAlign = "center";
+ctx.font = "40px Comic Sans MS";
 
-///////////////////// EVENT LISTENERS ///////////////////////////////
-window.onload = canvasLoad;
-///////////////////// FUNCTIONS /////////////////////////////////////
-function canvasLoad() {
-  document.getElementById("brick-canvas").onclick = canvasDraw;
+
+// Variables
+let dx;
+let dxFactor;
+let speed;
+let gameStarted = false;
+
+// Objects
+let bottles = [];
+
+let ball = {
+  x: undefined,
+  y: undefined,
+  radius: 10,
+  right: true,
+  up: true
+};
+
+let platform = {
+  x: (canvas.width / 2) - 40,
+  y: canvas.height - 10,
+  width: 80,
+  height: 5,
+  movement: 1
+};
+
+
+
+// Event Listeners
+window.onload = function() {
+  document.getElementById("brick-canvas").onclick = init;
+  game();
 }
-function canvasDraw() {
-  ctx.beginPath();
-  ctx.rect(20, 40, 50, 50);
-  ctx.fillStyle = "#FF0000";
-  ctx.fill();
-  drawBall();
-  draw();
-  ctx.closePath();
-}
-
-function drawBall() {
-  ctx.beginPath();
-  ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-  ctx.fillStyle = ballColor()
-  ctx.fill();
-  ctx.closePath();
-}
-
-function ballColor(colorBall) {
- if(colorBall == 1) {
-    colorBall = "#ff0000"
-  } else if(colorBall == 2) {
-     colorBall = "#ffa500"
-  } else if(colorBall == 3) {
-     colorBall = "#ffff00"
-  } else if(colorBall == 4) {
-      colorBall = "#008000"
-  } else if(colorBall == 5) {
-      colorBall = "#0000ff"
-  } else if(colorBall == 5) {
-      colorBall = "#0000ff"
-  } else if(colorBall == 6) {
-      colorBall = "#4b0082"
-  } else if(colorBall == 7) {
-      colorBall = "#ee82ee"
-  }
-}
-
-
-function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBall();
-
-    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
-        dx = -dx;
-        colorBall++
-    }
-    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
-        dy = -dy;
-       colorBall++
-    }
-
-    x += dx;
-    y += dy;
-}
-
-setInterval(draw, 10);
+document.addEventListener("keydown", getArrowKeys);
+document.getElementById("brick-breaker-game").addEventListener("mousemove", getMouse);
