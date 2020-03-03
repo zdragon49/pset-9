469e41ee9d34b9e35acde9c88c81c38680f0f26c
Lukas Mikulenas <lmikulenas@ucvts.org>
2020-03-02 23:31:00 -0500

    added the first steps

---
+++ js/app3.js
@@ -0,0 +1,22 @@
+var plays = "white";
+var header = document.getElementById("h2");
+header.textContent = "plays: white";
+var secondplays = false;
+var nowid = -1;
+var closede = false;
+var win = "";
+var amountplays = 0;
+var players = "Two";
+window.onload = function(){
+for (var i=0; i< 64; i++){
+    let dive = document.createElement("div");
+    dive.style.backgroundColor = parseInt((i / 8) + i) % 2 == 0 ? '#D2691E' : 'white';
+    dive.className = "pawn";
+	  dive.id = i;
+	  dive.innerHTML = parseInt((i / 8) + i) % 2 == 0 && (i < 24) ? '<div class="secondPlayer"></div>' : (parseInt((i / 8) + i) % 2 == 0 && (i > 40) ? '<div class="firstPlayer"></div>' : '');
+    document.getElementById("checkersBoard").appendChild(dive);
+}
+
+setClick();
+setReset();
+}
